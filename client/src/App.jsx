23360fe66b25efc1f774ba4bbbc2358872b7186c
Aman Kumar <aman228115918@gmail.com>
2025-01-29 17:11:36 +0530

    Login Page done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <main>
+        <Login />
+      </main>
+    </>
+  );
 }
 
 export default App;

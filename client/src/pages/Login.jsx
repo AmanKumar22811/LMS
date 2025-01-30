d88bb46838e2c9eb4455daecde11e15b7ba8945a
Aman Kumar <aman228115918@gmail.com>
2025-01-30 18:05:58 +0530

    build navbar

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -62,16 +62,16 @@ const Login = () => {
   };
   useEffect(() => {
     if (registerIsSuccess && registerData) {
-      toast.success(registerData.message || "Signup Successfully.");
+      toast.success(registerData?.message || "Signup Successful");
     }
     if (registerError) {
-      toast.error(registerData.data.message || "Signup Failed.");
+      toast.error(registerError?.data?.message || "Signup Failed");
     }
     if (loginIsSuccess && loginData) {
-      toast.success(loginData.message || "Login Successfully.");
+      toast.success(loginData?.message || "Login Successful");
     }
     if (loginError) {
-      toast.error(loginData.data.message || "Login Failed.");
+      toast.error(loginError?.data?.message || "Login Failed");
     }
   }, [
     loginIsLoading,

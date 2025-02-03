8ec04de965747362950fa3895b821fdfc91a4227
Aman Kumar <aman228115918@gmail.com>
2025-02-03 17:45:40 +0530

    fetch admin course done

--- client/src/pages/admin/course/AddCourse.jsx
+++ client/src/pages/admin/course/AddCourse.jsx
@@ -10,24 +10,34 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useCreateCourseMutation } from "@/features/api/courseApi";
 import { Loader2 } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const AddCourse = () => {
   const [courseTitle, setCourseTitle] = useState("");
   const [category, setCategory] = useState("");
+
+  const [createCourse, { data, error, isSuccess, isLoading }] =
+    useCreateCourseMutation();
   const navigate = useNavigate();
-  const isLoading = 0;
 
   const getSelectedCategory = (value) => {
     setCategory(value);
   };
 
   const createCourseHandler = async () => {
-    console.log(courseTitle, category);
+    await createCourse({ courseTitle, category });
   };
 
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success(data?.message || "Course created");
+      navigate("/admin/course");
+    }
+  }, [isSuccess, error]);
   return (
     <div className="flex-1 mx-10">
       <div className="mb-4">
--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import isAuthenticated from "../middelwares/isAuthenticated.middleware.js";
-import { createCourse } from "../controllers/course.controller.js";
+import {
+  createCourse,
+  getCreaterCourses,
+} from "../controllers/course.controller.js";
 
 const router = express.Router();
 
 router.route("/").post(isAuthenticated, createCourse);
+router.route("/").get(isAuthenticated, getCreaterCourses);
 
 export default router;
